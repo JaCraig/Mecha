894823aa00de6f3b7f72292e26dd692932fa6f17
James Craig <craig.jamesa@gmail.com>
2021-05-18 19:30:52 -0400

    - Added ArrayShrinker.

---
+++ Mecha.Core.Tests/Shrinker/Defaults/ArrayShrinkerTests.cs
@@ -0,0 +1,40 @@
+﻿using Mecha.Core.Shrinker.Defaults;
+using Mecha.Core.Tests.BaseClasses;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Mecha.Core.Tests.Shrinker.Defaults
+{
+    /// <summary>
+    /// Array shrinker tests
+    /// </summary>
+    public class ArrayShrinkerTests : TestBaseClass<ArrayShrinker>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArrayShrinkerTests"/> class.
+        /// </summary>
+        public ArrayShrinkerTests()
+        {
+            TestObject = new ArrayShrinker();
+        }
+
+        /// <summary>
+        /// RunAsync test
+        /// </summary>
+        [Fact]
+        public void CanShrink()
+        {
+            Assert.False(TestObject.CanShrink(new List<string>()));
+            Assert.True(TestObject.CanShrink(Array.Empty<string>()));
+        }
+
+        [Fact]
+        public void Shrink()
+        {
+            var Result = (string[])TestObject.Shrink(new string[] { "B", "A" });
+            Assert.Single(Result);
+            Assert.Equal("B", Result[0]);
+        }
+    }
+}
---
+++ Mecha.Core/Shrinker/Defaults/ArrayShrinker.cs
@@ -0,0 +1,44 @@
+﻿using Fast.Activator;
+using Mecha.Core.Shrinker.Interfaces;
+using System;
+
+namespace Mecha.Core.Shrinker.Defaults
+{
+    /// <summary>
+    /// Array shrinker
+    /// </summary>
+    /// <seealso cref="IShrinker"/>
+    public class ArrayShrinker : IShrinker
+    {
+        /// <summary>
+        /// Determines whether this instance can shrink.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns><c>true</c> if this instance can shrink; otherwise, <c>false</c>.</returns>
+        public bool CanShrink(object? value)
+        {
+            return !(value is null) && value.GetType().IsArray;
+        }
+
+        /// <summary>
+        /// Shrinks the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public object? Shrink(object? value)
+        {
+            if (value is null || !CanShrink(value))
+                return value;
+            var OriginalArray = (Array)value;
+            if (OriginalArray.Length == 0)
+                return value;
+            var NewLength = (4 * OriginalArray.Length) / 5;
+            var ArrayInstance = (Array)FastActivator.CreateInstance(value.GetType(), new object[] { NewLength });
+            for (var x = 0; x < NewLength; ++x)
+            {
+                ArrayInstance.SetValue(OriginalArray.GetValue(x), x);
+            }
+            return ArrayInstance;
+        }
+    }
+}
